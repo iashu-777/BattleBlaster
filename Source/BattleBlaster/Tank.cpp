7f4feebca2331225993aed9ea699e6f7088e3f25
Ashish <2k23.csds2310610@gmail.com>
2026-03-01 20:33:48 +0530

    Initial clean Unreal project start

---
+++ Source/BattleBlaster/Tank.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+
+ATank::ATank()
+{
+	SpringArmComp=CreateDefaultSubobject<USpringArmComponent>(TEXT("SprinArmComp"));
+	SpringArmComp->SetupAttachment(CapsuleComp);
+
+	CameraComp=CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		
+		if (ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
+		{
+			
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+		}
+	}
+}
+
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
