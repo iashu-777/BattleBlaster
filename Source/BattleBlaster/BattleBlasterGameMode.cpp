df70cd5a905e3a0196ddb402f46dbc31e1af336c
Ashish <2k23.csds2310610@gmail.com>
2026-03-06 11:01:48 +0530

    getting all enemies as an array elements

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -2,4 +2,42 @@
 
 
 #include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tower.h"
+void ABattleBlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
 
+	TArray<AActor*>Towers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATower::StaticClass(),Towers);
+	TowerCount = Towers.Num();
+	UE_LOG(LogTemp, Display, TEXT("Number of towers: %d"), TowerCount);
+
+	APawn* PlayerPawn=UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn)
+	{
+		Tank = Cast<ATank>(PlayerPawn);
+		if (!Tank)
+		{
+			UE_LOG(LogTemp, Display, TEXT("GameMode: Failed to find the tank actor!"));
+		}
+	}
+
+	UE_LOG(LogTemp, Display, TEXT("loop starts here!"));
+	int32 LoopIndex = 0;
+	while (LoopIndex < TowerCount)
+	{
+		AActor* TowerActor=Towers[LoopIndex];
+		if (TowerActor)
+		{
+		ATower* Tower= Cast<ATower>(TowerActor);
+		if (Tower)
+		{
+			Tower->Tank = Tank;
+			UE_LOG(LogTemp, Display, TEXT("%s setting the tank variable!"),*Tower->GetActorNameOrLabel());
+		}
+		}
+		LoopIndex++;
+	}
+	UE_LOG(LogTemp, Display, TEXT("loop ends here!"));
+}
--- Source/BattleBlaster/Tower.h
+++ Source/BattleBlaster/Tower.h
@@ -3,25 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "BasePawn.h"
 
 #include "Tank.h"
 
-#include "BattleBlasterGameMode.generated.h"
+#include "Tower.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLEBLASTER_API ABattleBlasterGameMode : public AGameModeBase
+class BATTLEBLASTER_API ATower : public ABasePawn
 {
 	GENERATED_BODY()
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 public:
+
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
 	ATank* Tank;
-	int32 TowerCount;
 };
