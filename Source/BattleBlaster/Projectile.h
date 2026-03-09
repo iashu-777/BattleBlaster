9702bab708014ccb58b993be89bf99cd023f3703
Ashish <2k23.csds2310610@gmail.com>
2026-03-09 14:20:23 +0530

    added health component in tower  and tank

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -30,6 +30,9 @@ public:
 	UPROPERTY(VisibleAnywhere, Category = Movement)
 	UProjectileMovementComponent* ProjectileMovementComponent;
 
+	UPROPERTY(EditAnywhere)
+	float Damage = 25.0f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
